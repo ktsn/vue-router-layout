967fd02b158583ba12510e4126c9b51e4d67f2e4
ktsn <ktsn55@gmail.com>
2018-07-13 23:34:56 +0900

    initial implementation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,54 @@
-export default {
-  // this file is the entry point of the project
+import Vue, { VueConstructor, VNode, AsyncComponent } from 'vue'
+
+export function createLayoutSelector(
+  resolve: (layoutName: string) => AsyncComponent
+): VueConstructor {
+  return Vue.extend({
+    name: 'LayoutSelector',
+
+    data() {
+      return {
+        layoutName: undefined as string | undefined,
+        layouts: Object.create(null) as Record<string, AsyncComponent>
+      }
+    },
+
+    beforeRouteEnter(to, _from, next) {
+      const reversed = to.matched.slice().reverse()
+
+      let layoutName = 'default'
+      for (const record of reversed) {
+        const Component: any = record.components.default
+        if (Component) {
+          const name =
+            typeof Component === 'function'
+              ? Component.options.layout
+              : Component.layout
+
+          if (name) {
+            layoutName = name
+            break
+          }
+        }
+      }
+
+      next((vm: any) => {
+        vm.layoutName = layoutName
+
+        if (!vm.layouts[layoutName]) {
+          vm.$set(vm.layouts, layoutName, resolve(layoutName))
+        }
+      })
+    },
+
+    render(h): VNode {
+      const layout = this.layoutName && this.layouts[this.layoutName]
+      if (!layout) {
+        return h()
+      }
+      return h(layout, {
+        key: this.layoutName
+      })
+    }
+  })
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,9 +1,106 @@
-import * as assert from 'power-assert'
-import index from '../src/index'
+import { mount as _mount } from '@vue/test-utils'
+import Vue, { Component } from 'vue'
+import Router, { RouteConfig } from 'vue-router'
+import { createLayoutSelector } from '../src/index'
 
-describe('Entry point', () => {
-  it('should provide module', () => {
-    const actual = index
-    assert.deepStrictEqual(actual, {})
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+Vue.use(Router)
+
+const layouts: Record<string, Component> = {
+  default: {
+    template: '<div>Default <router-view/></div>'
+  },
+
+  foo: {
+    template: '<div>Foo <router-view/></div>'
+  },
+
+  bar: {
+    template: '<div>Bar <router-view/></div>'
+  }
+}
+
+const LayoutSelector = createLayoutSelector(layout => {
+  return () => Promise.resolve(layouts[layout])
+})
+
+async function mount(children: RouteConfig[]) {
+  const router = new Router({
+    routes: [
+      {
+        path: '/',
+        component: LayoutSelector,
+        children
+      }
+    ]
+  })
+
+  const Root = Vue.extend({
+    template: '<router-view/>'
+  })
+
+  const wrapper = _mount(Root, {
+    router
+  })
+  await wrapper.vm.$nextTick()
+  return wrapper
+}
+
+describe('LayoutSelector component', () => {
+  it('shows default layout', async () => {
+    const Comp = {
+      template: '<p>component</p>'
+    }
+
+    const wrapper = await mount([
+      {
+        path: '',
+        component: Comp
+      }
+    ])
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('shows named layout', async () => {
+    const Comp = {
+      layout: 'foo',
+      template: '<p>component</p>'
+    }
+
+    const wrapper = await mount([
+      {
+        path: '',
+        component: Comp
+      }
+    ])
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it("uses the leaf component's layout option", async () => {
+    const Test1 = {
+      layout: 'foo',
+      template: '<div>Test1 <router-view/></div>'
+    }
+
+    const Test2 = {
+      layout: 'bar',
+      template: '<p>Test2</p>'
+    }
+
+    const wrapper = await mount([
+      {
+        path: '',
+        component: Test1,
+        children: [
+          {
+            path: '',
+            component: Test2
+          }
+        ]
+      }
+    ])
+    expect(wrapper.html()).toMatchSnapshot()
   })
 })
